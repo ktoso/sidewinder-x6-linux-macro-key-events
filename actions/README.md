8f946ce406bf71fbd30e8bb47ae26e590ee8ec83
Konrad Malawski <konrad.malawski@project13.pl>
2011-07-27 23:35:12 +0200

    empty actions

---
+++ actions/README.md
@@ -0,0 +1,9 @@
+What's this?
+------------
+
+These files will be interpreted when one of your Sidewinder's macro keys
+are fired. Just type in some **xdotool** lines into them, to have your macro
+keys map exactly how you want them to :-)
+
+Cheers!
+Ktoso
---
+++ actions/S29.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Example code, using xdotool ( you may need to install it: http://www.semicomplete.com/projects/xdotool/ )
+# on fedora: sudo yum install xdotool
+# on ubuntu: sudo apt-get install xdootool
+#
+# WID=`xdotool search "Mozilla Firefox" | head -1`
+# xdotool windowactivate --sync $WID
+# xdotool key --clearmodifiers ctrl+l
