9984b47ab1f0a2db4eeb87acf4c1f8b9d0e8c1cd
Konrad Malawski <konrad.malawski@project13.pl>
2011-07-27 23:20:02 +0200

    all S1 - S12 event keys are working now :) There are problems with S13-S30 as they need to get some magic interrupt from the windows driver...

--- event-gen.rb
+++ event-gen.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/env ruby
 
 ARGF.each do |line|
-#    puts "Got: " + line #if line =~ /login/
+    puts "Got: " + line #if line =~ /login/
 
-    puts "S1 -" if line =~ /08000000 00$/
-    puts "S1 _" if line =~ /08010000 00$/
+    puts "____" if line =~ /08000000 00$/
+
+    puts "S01 -" if line =~ /08010000 00$/
+    puts "S02 -" if line =~ /08020000 00$/
+    puts "S03 -" if line =~ /08040000 00$/
+    puts "S04 -" if line =~ /08080000 00$/
+    puts "S05 -" if line =~ /08100000 00$/
+    puts "S06 -" if line =~ /08200000 00$/
+
+    puts "S07 -" if line =~ /08400000 00$/
+    puts "S08 -" if line =~ /08800000 00$/
+    puts "S09 -" if line =~ /08000100 00$/
+    puts "S10 -" if line =~ /08000200 00$/
+    puts "S11 -" if line =~ /08000400 00$/
+    puts "S12 -" if line =~ /08000800 00$/
 end
 
