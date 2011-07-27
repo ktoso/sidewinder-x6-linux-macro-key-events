30b530d2e2fa5a5cf4e4161e08f9fa3cd1781c9f
Konrad Malawski <konrad.malawski@project13.pl>
2011-07-27 23:46:26 +0200

    it's now delegating to run the actions :)

--- event-gen.rb
+++ event-gen.rb
@@ -1,22 +1,38 @@
 #!/usr/bin/env ruby
 
-ARGF.each do |line|
-    puts "Got: " + line #if line =~ /login/
+home = '/home/ktoso/coding/sidewinder-x6-linux-macro-key-events'
 
+ARGF.each do |line|
     puts "____" if line =~ /08000000 00$/
 
-    puts "S01 -" if line =~ /08010000 00$/
-    puts "S02 -" if line =~ /08020000 00$/
-    puts "S03 -" if line =~ /08040000 00$/
-    puts "S04 -" if line =~ /08080000 00$/
-    puts "S05 -" if line =~ /08100000 00$/
-    puts "S06 -" if line =~ /08200000 00$/
+    if line =~ /08010000 00$/
+      %x["#{home}/actions/S01.sh"]
+    elsif line =~ /08020000 00$/
+      %x["#{home}/actions/S02.sh"]
+    elsif line =~ /08040000 00$/
+      %x["#{home}/actions/S03.sh"]
+    elsif line =~ /08080000 00$/
+      %x["#{home}/actions/S04.sh"]
+    elsif line =~ /08100000 00$/
+      %x["#{home}/actions/S05.sh"]
+    elsif line =~ /08200000 00$/
+      %x["#{home}/actions/S06.sh"]
+    elsif line =~ /08400000 00$/
+      %x["#{home}/actions/S07.sh"]
+    elsif line =~ /08800000 00$/
+      %x["#{home}/actions/S08.sh"]
+    elsif line =~ /08000100 00$/
+      %x["#{home}/actions/S09.sh"]
+    elsif line =~ /08000200 00$/
+      %x["#{home}/actions/S10.sh"]
+    elsif line =~ /08000400 00$/
+      %x["#{home}/actions/S11.sh"]
+    elsif line =~ /08000800 00$/
+      %x["#{home}/actions/S12.sh"]
+#    else
+#      puts "unrecognized code: " + line
+    end
 
-    puts "S07 -" if line =~ /08400000 00$/
-    puts "S08 -" if line =~ /08800000 00$/
-    puts "S09 -" if line =~ /08000100 00$/
-    puts "S10 -" if line =~ /08000200 00$/
-    puts "S11 -" if line =~ /08000400 00$/
-    puts "S12 -" if line =~ /08000800 00$/
+
 end
 
